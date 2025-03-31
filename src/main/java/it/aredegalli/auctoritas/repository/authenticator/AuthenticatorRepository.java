789707a5f106189641625f3b6dabbec0c30e452f
AEndrix <redegalli.andrea03@gmail.com>
2025-04-01 01:43:09 +0200

    fix

--- src/main/java/it/aredegalli/auctoritas/repository/authenticator/AuthenticatorRepository.java
+++ src/main/java/it/aredegalli/auctoritas/repository/authenticator/AuthenticatorRepository.java
@@ -6,5 +6,8 @@ import it.aredegalli.auctoritas.repository.UUIDRepository;
 import java.util.Optional;
 
 public interface AuthenticatorRepository extends UUIDRepository<Authenticator> {
+
+    boolean existsByName(String name);
+
     Optional<Authenticator> findByName(String name);
 }
